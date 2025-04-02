e245ca65bd91d082ad3c1e52035b34f91d86439f
Tania2007809 <tania.rodriguez5a26@gmail.com>
2025-04-01 22:58:57 -0600

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// filepath: .eslintrc.js
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  };
